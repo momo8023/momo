4fd9920cf6136bad1c3776435e5aeb53a04a18e2
momo <492214045@qq.com>
2019-04-23 16:44:55 +0800

    Initial commit

---
+++ src/main/java/com/momo/video/controller/indexController.java
@@ -0,0 +1,22 @@
+package com.momo.video.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class indexController {
+
+    /**
+     * 跳转到首页
+     *
+     * @return
+     */
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+
+        return "videos_open";
+    }
+
+
+}
